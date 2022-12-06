cce87fd76e8a371b2914839df3745d279c02c4d5
kaisei <c0a2102368@edu.teu.ac.jp>
2022-12-06 14:44:11 +0900

    3-7

--- ex03/maze.py
+++ ex03/maze.py
@@ -7,6 +7,20 @@ def key_up(event):
     global key
     key=""
 
+def main_proc():
+    global cx,cy
+    if key=="Up":
+        cy-=20
+    if key=="Down":
+        cy+=20
+    if key=="Left":
+        cx-=20
+    if key=="Right":
+        cx+=20
+    #koukatonの画像変更
+    canvas.coords("koukaton",cx,cy)
+    root.after(100,main_proc)
+
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -21,4 +35,5 @@ if __name__ == "__main__":
     key=""
     root.bind("<KeyPress>",key_down)
     root.bind("<KeyRelease>",key_up)
+    main_proc()
     root.mainloop()
